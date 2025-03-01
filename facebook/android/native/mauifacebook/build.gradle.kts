78a97b02220b8af32d428b75f14b00ae73335b95
Oleksii Burianov <alexeyburyanov@gmail.com>
2025-03-01 17:31:05 +0200

    Test

---
+++ facebook/android/native/mauifacebook/build.gradle.kts
@@ -0,0 +1,33 @@
+plugins {
+    id("com.android.library")
+}
+
+android {
+    namespace = "com.microsoft.mauifacebook"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 21
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+}
+
+dependencies {
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.11.0")
+    implementation("com.facebook.android:facebook-android-sdk:17.0.2")
+}
+
