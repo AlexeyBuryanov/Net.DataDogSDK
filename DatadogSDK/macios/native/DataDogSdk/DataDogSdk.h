2db3378c0e4068abd009e5714219cfd378c27fde
Alexey Buryanov <alexeyburyanov@gmail.com>
2025-03-02 13:04:18 +0200

    WIP iOS

---
+++ DatadogSDK/macios/native/DataDogSdk/DataDogSdk.h
@@ -0,0 +1,7 @@
+#import <Foundation/Foundation.h>
+
+//! Project version number for DataDogSdk.
+FOUNDATION_EXPORT double DataDogSdkVersionNumber;
+
+//! Project version string for DataDogSdk.
+FOUNDATION_EXPORT const unsigned char DataDogSdkVersionString[];
